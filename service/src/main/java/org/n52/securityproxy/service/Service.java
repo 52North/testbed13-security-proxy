2038ae081a3cb1b3390113b65cd97888578d8700
staschc <staschc@uni-muenster.de>
2017-08-01 14:08:27 +0200

    licensing and dependency issues resolved

--- service/src/main/java/org/n52/securityproxy/service/Service.java
+++ service/src/main/java/org/n52/securityproxy/service/Service.java
@@ -42,11 +42,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Service implements ServletContextAware, ServletConfigAware {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Service.class);
-    
+
     private ServletContext ctx;
-    
+
     private ObjectMapper m;
-  
+
     public void init() {
 
         m = new ObjectMapper();
@@ -55,28 +55,28 @@ public class Service implements ServletContextAware, ServletConfigAware {
     @RequestMapping(value = "{serviceId}", method = RequestMethod.GET)
     public void get(@PathVariable String serviceId, HttpServletRequest req,
             HttpServletResponse res) throws Exception{
-        
+
         LOGGER.info("Incoming request for service with id: " + serviceId);
-        
+
         String token = req.getHeader("Authorization");
-        
+
         if(token == null){
             LOGGER.info("No token was send.");
             return;
         }
-        
+
         DecodedJWT jwt = new TokenDecoder(ctx.getResourceAsStream("/WEB-INF/pubkey/pubkey.pem")).decodeToken(token, "https://bpross-52n.eu.auth0.com/");
-        
+
         String base64EncodedPayload = jwt.getPayload();
-        
+
         byte[] decodedPayloadByteArray = Base64.getDecoder().decode(base64EncodedPayload);
-        
+
         JsonNode rootNode = m.readTree(new String(decodedPayloadByteArray));
-        
+
         JsonNode scopeNode = rootNode.findPath("scope");
-        
+
         String scopeText = scopeNode.textValue();
-        
+
         LOGGER.info(scopeText);
     }
 
